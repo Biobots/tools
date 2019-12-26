8e95158a16d6e2cc1b6e9de1d49b138d7e87fda9
biobot <biobot94@gmail.com>
2019-12-26 22:01:44 +0800

    try sth
    Signed-off-by: biobot <biobot94@gmail.com>

--- pt/u2/u2.py
+++ pt/u2/u2.py
@@ -1,4 +1,17 @@
 from utils import *
+import os
+
+def printOption(trtlist, selected):
+	index = 0
+	for i in torrent:
+		color = ''
+		if index == selected:
+			color = '\033[1;32m'
+		print(color+str(index)+': '+i.title)
+		print('  体积: '+i.size+'  优惠: UP:'+str(i.promo.upload)+'| DOWN'+str(i.promo.download)+'\033[0m\n')
+		index += 1
+	index -= 1
+	return
 
 #ret = requests.get(url=config['url']+"/torrents.php", cookies=config['cookies'], headers=config['headers'])
 #rst = searchTorrents(ret.text)
@@ -17,7 +30,8 @@ while (count <= num):
 		torrent.append(Torrent(getTitle(i), id, getPromo(i), url, getSize(i)))
 		count += 1
 	pagecount += 1
-for i in torrent:
-	print(i.title)
-	print(i.detailurl)
-	print(i.size)
+command = 0
+while True:
+	os.system('clear')
+	printOption(torrent, command)
+	command = int(input("choose one:"))
